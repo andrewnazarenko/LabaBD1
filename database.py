14d539d2c0784aed8788cf42c1b7785c7ad0150d
andrewnazarenko <sentix.ua@gmail.com>
2016-09-16 16:07:31 +0300

    added backup

--- database.py
+++ database.py
@@ -1,20 +1,25 @@
-import tovar
+import tovar as tovar
 import tipTovara as tip
-
+import pickle
 d1 = {}
 
 
 def view_types():
-    print(d1.keys())
+    for i in d1.keys():
+        print i
 
 
 def view_tovary():
-    return None
+    for i in sum(d1.values(), []):
+        print i
+
 
 
 def view_all():
-    return None
-
+    for i in d1.keys():
+        print i
+        for k in d1[i]:
+            print "  " + str(k)
 
 def add_types():
     name = str(raw_input("Type name: "))
@@ -25,12 +30,22 @@ def add_types():
 
 
 def add_tovary():
-    return None
-
+    name = str(raw_input("Type name: "))
+    price = str(raw_input("Type price"))
+    date = str(raw_input("Type date"))
+    tovary = tovar.tovar(name, price, date)
+    tovartype = d1.keys()
+    for i in range(len(tovartype)):
+        print str(i)+". " + str(tovartype[i])
+    type1 = int(raw_input("Type category num:"))
+    if d1[tovartype[type1]] == None:
+        d1[tovartype[type1]] = [tovary]
+    else:
+        d1[tovartype[type1]].append(tovary)
 
 def del_types():
-    return None
-
+    for i in d1.keys():
+        del(i)
 
 def del_tovary():
     return None
@@ -42,3 +57,17 @@ def edit_types():
 
 def edit_tovary():
     return None
+
+
+def save_into_file_please():
+    na_file = open('backup.txt', 'w')
+    pickle.dump(d1, na_file)
+    na_file.close()
+
+
+def load_from_file_please():
+    global d1
+    na_file = open('backup.txt', 'r')
+    d1 = pickle.load(na_file)
+    na_file.close()
+
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import menu
+import database
 
+database.load_from_file_please()
 menu.main_menu()
+database.save_into_file_please()
